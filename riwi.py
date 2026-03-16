eb599a9302c39573631af5b17c3b2a5c6c6c58ba
camilomeza14 <cmeza555@hotmail.com>
2026-03-16 13:26:40 -0500

    Implement genre registration for movies
    Add functionality to register and count movie genres based on user input.

--- riwi.py
+++ riwi.py
@@ -1,1 +1,12 @@
-b  bn nb 
+pelicula = 0 
+accion = 0 
+comedia = 0
+for i in range(5): # Registrar 5 clientes
+    genero = input("Elija género (pelicula, accion, comedia): ").lower()
+    if genero == "pelicula": pelicula += 1
+    elif genero == "accion": accion += 1
+    elif genero == "comedia": comedia += 1
+
+print(f"Películas: {pelicula}")
+print(f"Acción: {accion}")
+print(f"Comedia: {comedia}") 
